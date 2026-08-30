41d0a3f6a5279dd853c905f8acd44992ccb923ab
miumigy <209484393+miumigy@users.noreply.github.com>
2026-08-30 08:34:58 +0000

    chore(autofix): apply ruff --fix and black

--- alembic/versions/4eab49599376_add_plan_job_id_to_runs_table.py
+++ alembic/versions/4eab49599376_add_plan_job_id_to_runs_table.py
@@ -6,9 +6,9 @@ Create Date: 2025-10-03 14:00:00
 
 """'''
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "4eab49599376"
